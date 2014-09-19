f6357b01f8189fc14d5def023ef7fa03919b3013
FrankThePlank <christia.gicas@techhigh.us>
2014-09-19 07:48:14 -0400

    Added comment

--- DrawFigures1.java
+++ DrawFigures1.java
@@ -1,5 +1,6 @@
 public class DrawFigures1 {
-    public static void main(String[] args) {
+// Shrek is love, Shrek is life   
+   public static void main(String[] args) {
         System.out.println("   /\\");
         System.out.println("  /  \\");
         System.out.println(" /    \\");
